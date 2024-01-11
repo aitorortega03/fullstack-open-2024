4cca1a4ccd5c866487b81c37dba99e01d3947b55
aitorortega03 <aitorortega21@gmail.com>
2024-01-11 20:00:51 +0100

    refactor exercise 1.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,17 +1,14 @@
-const Content = (props) => {
-    return ( 
-        <div>
-            <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-        </div>
-     );
-}
- 
+import Part from "./Part";
+
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+  
+  return (
+    <div>
+      <Part part = {part1} exercises={exercises1}/>
+      <Part part = {part2} exercises={exercises2}/>
+      <Part part = {part3} exercises={exercises3}/>
+    </div>
+  );
+};
+
 export default Content;
