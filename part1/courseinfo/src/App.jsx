70d40e08e29599721c375d4419a337db62ae070d
aitorortega03 <aitorortega21@gmail.com>
2024-01-11 22:38:43 +0100

    fix refactor exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -22,10 +22,10 @@ const App = () => {
       <Header course={course} />
       <Content
         part1={part1?.name}
-        part2={part2.name}
-        part3={part3.name}
+        part2={part2?.name}
+        part3={part3?.name}
         exercises1={part1?.exercises}
-        exercises2={part2?.part1?.exercises}
+        exercises2={part2?.exercises}
         exercises3={part3?.exercises}
       />
       <Total
