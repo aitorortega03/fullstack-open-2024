eddfdfd3a6c08c6c81319df324f5c0ef05f490c0
aitorortega03 <aitorortega21@gmail.com>
2024-01-22 21:31:41 +0100

    refactor state and done clicks

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,25 +1,34 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleGoodClicks = () =>
+    setClicks({ ...clicks, good: clicks.good + 1 });
+
+  const handleNeutralClicks = () =>
+    setClicks({ ...clicks, neutral: clicks.neutral + 1 });
+
+  const handleBadClicks = () => setClicks({ ...clicks, bad: clicks.bad + 1 });
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={handleGoodClicks}>good</button>
+      <button onClick={handleNeutralClicks}>neutral</button>
+      <button onClick={handleBadClicks}>bad</button>
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {clicks.good}</p>
+      <p>neutral {clicks.neutral}</p>
+      <p>bad {clicks.bad}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
