88e30bd246163fe57794d3f5f20be725fd7e578b
aitorortega03 <aitorortega21@gmail.com>
2024-01-24 22:49:44 +0100

    add conditional rendering exercise 1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,13 +1,11 @@
 const Statistics = ({ clicks }) => {
+
   const calculateAverage = () => clicks.good * 1 + clicks.bad * -1;
 
   const calculateTotalComments = () =>
     clicks.good + clicks.neutral + clicks.bad;
 
     const calculatePositivePercentaje = () => {
-        // clicks.good + clicks.neutral + clicks.bad === 0
-        //   ? 0
-        //   : clicks.good / calculateTotalComments()
         if (clicks.good + clicks.neutral + clicks.bad === 0) {
             return 0
         }
@@ -16,6 +14,14 @@ const Statistics = ({ clicks }) => {
         }
 
         return clicks.good / calculateTotalComments()
+    }
+
+    if (clicks.good === 0 && clicks.neutral === 0 && clicks.bad === 0) {
+      return (
+        <div>
+          <p>No feedback given</p>
+        </div>
+      )
     }
 
   return (
