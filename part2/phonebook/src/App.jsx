bcbfd4a83601d21cf9c8e262a8b47aae03e5e08c
aitorortega03 <aitorortega21@gmail.com>
2024-02-20 15:39:54 +0100

    finish refactor 2.13 and delete person exercise 2.14

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import axios from 'axios'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personService from "./services/personService";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,9 +11,8 @@ const App = () => {
   const [nameFilter, setNameFilter] = useState('')
 
   useEffect(() => {
-    console.log('useEffect')
-    axios.get('http://localhost:3001/persons')
-    .then(response => setPersons(response.data))
+    personService.getAllPersons()
+    .then(initialPersons => setPersons(initialPersons))
   }, [])
 
   const addName = (event) => {
@@ -25,9 +24,9 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
     } else {
-      axios.post('http://localhost:3001/persons', newPerson)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService.createPerson(newPerson)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
         })
@@ -41,7 +40,7 @@ const App = () => {
       <h3>add a new</h3>
       <PersonForm addName={addName} newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} />
       <h3>Numbers</h3>
-      <Persons persons={persons} nameFilter={nameFilter} />
+      <Persons persons={persons} nameFilter={nameFilter} setPersons={setPersons}/>
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,17 +1,28 @@
-const Persons = ({ persons, nameFilter }) => {
-    return ( 
-        <ul>
-        {persons
-          .filter((person) =>
-            person.name.toLowerCase().includes(nameFilter.toLowerCase())
-          )
-          .map((person) => (
-            <li key={person.name}>
-              {person.name} {person.number}
-            </li>
-          ))}
-      </ul>
-     );
-}
- 
+import personService from "../services/personService";
+
+const Persons = ({ persons, nameFilter, setPersons }) => {
+  const handleDelete = (person) => {
+
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      personService.deletePerson(person.id)
+      .then(personDeleted => setPersons(persons.filter(person => person.id !== personDeleted.id)))
+    }
+  };
+
+  return (
+    <ul>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(nameFilter.toLowerCase())
+        )
+        .map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number} {}
+            <button onClick={() => handleDelete(person)}>delete</button>
+          </li>
+        ))}
+    </ul>
+  );
+};
+
 export default Persons;
