151de5a2c0dbc9ce83e1dd591e346536ffdb275e
aitorortega03 <aitorortega21@gmail.com>
2024-03-05 20:09:21 +0100

    done capital weather

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,7 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Country = ({ country }) => {
+
+    const [cityWeather, setCityWeather] = useState({})
+
+    useEffect(() => {
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${import.meta.env.VITE_API_KEY}`)
+          .then(response => setCityWeather(response.data))
+      }, [country.capital]);
+
     return ( 
         <div>
-            <h2>{country.name.common}</h2>
+            <h1>{country.name.common}</h1>
             capital: {country.capital} <br />
             area: {country.area}
             <h3>languages:</h3>
@@ -11,6 +23,10 @@ const Country = ({ country }) => {
                 ))}
             </ul>
             <img src={country.flags.png} alt={country.flags.alt} />
+            <h2>Weather in {country.capital}</h2>
+            temperature {(cityWeather.main.temp - 273).toFixed(2)} Celsius
+            <br />
+            wind {cityWeather.wind.gust} m/s
         </div>
      );
 }
