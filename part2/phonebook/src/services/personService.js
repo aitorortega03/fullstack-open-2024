7192b3f23b5112498bec997f77a010dbf4d7063d
aitorortega03 <aitorortega21@gmail.com>
2024-03-26 17:01:35 +0100

    edit front url and update git ignore

--- part2/phonebook/src/services/personService.js
+++ part2/phonebook/src/services/personService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://phonebook-backend-kzef.onrender.com/api/persons";
 
 const getAllPersons = () => {
   return axios.get(baseUrl).then((response) => response.data);
