39dda68e0ad4f6a12471a3aa5064ef447ece2c91
aitorortega03 <aitorortega21@gmail.com>
2024-04-11 21:22:15 +0200

    add phone validation exercise 3.20

--- part3/phonebook-backend/src/models/person.js
+++ part3/phonebook-backend/src/models/person.js
@@ -22,7 +22,16 @@ const personSchema = new mongoose.Schema({
       minLength: 3,
       required: true
   },
-    number: String
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^[0-9]{2,3}-[0-9]+$/i.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number`
+      }
+    }
 })
 
 personSchema.set('toJSON', {
