ce81cc1148f6cffd7903932e7bd62f8ea069ab90
aitorortega03 <aitorortega21@gmail.com>
2024-04-14 15:26:12 +0200

    refactor exercise 4.2

---
+++ part4/blog-backend/src/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl)
+  .then((result) => {
+    logger.info("connected to MongoDB")
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.use('/api/blogs', notesRouter)
+
+module.exports = app
