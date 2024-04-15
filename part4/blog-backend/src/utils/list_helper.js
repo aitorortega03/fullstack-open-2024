4694f58b5f64bb21e8df7e57d2b702fe51faba85
aitorortega03 <aitorortega21@gmail.com>
2024-04-15 21:05:39 +0200

    finish exercise 4.5

--- part4/blog-backend/src/utils/list_helper.js
+++ part4/blog-backend/src/utils/list_helper.js
@@ -6,6 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const blog = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ?
+      prev : current
+  }, { likes: 0 })
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
